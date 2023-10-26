b433d16d34f90d4999420258224e54231d9191c5
Critiek <jumbra99@gmail.com>
2023-10-26 19:17:36 +1030

    working on DB

--- src/main.py
+++ src/main.py
@@ -4,4 +4,5 @@ import yaml
 with open('db/gear.yaml') as file:
     gear = yaml.safe_load(file)
 
-print(gear['hemlets']['ascended']['berserker'])
+# print(gear['hemlets']['ascended']['berserker'])
+print(gear)
