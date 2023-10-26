6c12f5a462d7c1abcbfe7caaf117cd30870da5e0
Critiek <jamesvsomeren@gmail.com>
2023-10-26 22:23:57 +1030

    input stuff for formula and formatted some code

--- src/main.py
+++ src/main.py
@@ -4,6 +4,23 @@ import yaml
 with open('db/gear.yaml') as file:
     gear = yaml.safe_load(file)
 
+power = 1000
+precision = 1000
+ferocity = 0
+
+crit_chance = (precision - 895) / 21
+critical_damage = (ferocity / 1500) + 1.5
+skill_coefficient = 2
+
+armour = int(input('How much armour does the target have? \n> '))
+weapon_max_damage = int(input('How much damage does your weapon do? \n> '))
+
+
+damage = ((power * weapon_max_damage * skill_coefficient) / armour) * (1 + crit_chance * (critical_damage - 1))
+
+print('you will deal [{}] damage to the target'.format(damage))
+
+
 # print(gear['hemlets']['ascended']['berserker'])
 # print(gear)
 
@@ -13,19 +30,9 @@ with open('db/gear.yaml') as file:
 #             for armour_stat in gear[armour_type][armour_rarity][armour_prefix]:
 #                 print(str(armour_type) + ': ' + str(armour_prefix) + str(armour_stat))
 
-power = 2600
-skill_coefficient = 2
-weapon_max_damage = 1100
-armour = 2000
-crit_chance = 0.05
-ferocity = 1.5
-
-damage = ((power * weapon_max_damage * skill_coefficient) / armour) * (1 + crit_chance * (ferocity - 1))
-
-print(damage)
 
 
-
+# NOTE: stuff below is just formula
 
 # ((power * 1100 * 2.0) / armour ) * (1 + crit_chance (ferocity - 1))
 
